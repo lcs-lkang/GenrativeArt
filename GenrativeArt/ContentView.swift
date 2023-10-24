ac59c7cfeb33a2e9fbe22be38130cd5f8a1cf751
Lawrence Kang <lkang@lcs.on.ca>
2023-10-24 11:31:05 -0400

    Initial Commit

---
+++ GenrativeArt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GenrativeArt
+//
+//  Created by Chen-Ning Kang on 2023-10-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GenrativeArt/GenrativeArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenrativeArtApp.swift
+//  GenrativeArt
+//
+//  Created by Chen-Ning Kang on 2023-10-24.
+//
+
+import SwiftUI
+
+@main
+struct GenrativeArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
