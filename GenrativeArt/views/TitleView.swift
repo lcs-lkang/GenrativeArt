62645950cd15b5d1818f6f24fe41be6362acef33
Lawrence Kang <lkang@lcs.on.ca>
2023-10-24 11:45:21 -0400

    Organized files and groups in the project.

---
+++ GenrativeArt/views/TitleView.swift
@@ -0,0 +1,18 @@
+//
+//  TitleView.swift
+//  GenrativeArt
+//
+//  Created by Chen-Ning Kang on 2023-10-24.
+//
+
+import SwiftUI
+
+struct TitleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TitleView()
+}
