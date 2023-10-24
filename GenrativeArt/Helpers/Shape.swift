7b4388f5abf86addb4e1fcf9b4d90b8ea1002aba
Lawrence Kang <lkang@lcs.on.ca>
2023-10-24 12:22:07 -0400

    organized. Shapes into own file.

---
+++ GenrativeArt/Helpers/Shape.swift
@@ -0,0 +1,89 @@
+//
+//  Shape.swift
+//  GenrativeArt
+//
+//  Created by Chen-Ning Kang on 2023-10-24.
+//
+
+import SwiftUI
+struct TriangleBottomRight: Shape {
+    func path(in rect: CGRect) -> Path {
+        
+        //Make path
+        var path = Path()
+        
+        //Define path
+        path.move(to: CGPoint(x: rect.maxX, y: rect.minY))
+        
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+        //Return path
+        return path
+        
+        
+    }
+    
+}
+
+struct TriangleTopLeft: Shape {
+    func path(in rect: CGRect) -> Path {
+        
+        //Make path
+        var path = Path()
+        
+        //Define path
+        path.move(to: CGPoint(x: rect.minX, y: rect.minY))
+        
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.minY))
+        //Return path
+        return path
+        
+        
+    }
+    
+}
+
+
+struct TriangleBottomLeft: Shape {
+    func path(in rect: CGRect) -> Path {
+        
+        //Make path
+        var path = Path()
+        
+        //Define path
+        path.move(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        //Return path
+        return path
+        
+        
+    }
+    
+}
+
+
+struct TriangleTopRight: Shape {
+    func path(in rect: CGRect) -> Path {
+        
+        //Make path
+        var path = Path()
+        
+        //Define path
+        path.move(to: CGPoint(x: rect.maxX, y: rect.minY))
+        
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+        //Return path
+        return path
+        
+        
+    }
+    
+}
