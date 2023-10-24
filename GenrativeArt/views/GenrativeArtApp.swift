f120052f2bbf3ecc8717bc70962d0acb14aacb86
Lawrence Kang <lkang@lcs.on.ca>
2023-10-24 12:02:32 -0400

    got top right triangle code

--- GenrativeArt/views/GenrativeArtApp.swift
+++ GenrativeArt/views/GenrativeArtApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct GenrativeArtApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TitleView()
         }
     }
 }
--- GenrativeArt/views/TitleView.swift
+++ GenrativeArt/views/TitleView.swift
@@ -7,12 +7,37 @@
 
 import SwiftUI
 
-struct TitleView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct TriangleTopRight: Shape {
+    func path(in rect: CGRect) -> Path {
+        
+        //Make path
+        var path = Path()
+        
+        //Define path
+        path.move(to: CGPoint(x: rect.maxX, y: rect.minY))
+        
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+        //Return path
+        return path
+        
+        
     }
+    
 }
 
+
+struct TitleView: View {
+    var body: some View {
+        VStack{
+            TriangleTopRight()
+            .aspectRatio(1.0, contentMode: .fit)
+        }
+        .padding()
+    }
+
+}
 #Preview {
     TitleView()
 }
