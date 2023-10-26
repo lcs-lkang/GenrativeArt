d9bf3a250921e521cb0799017dda768200c9114c
Lawrence Kang <lkang@lcs.on.ca>
2023-10-26 14:45:42 -0400

    code of swuare fill with color

--- GenrativeArt/Helpers/Shape.swift
+++ GenrativeArt/Helpers/Shape.swift
@@ -14,7 +14,6 @@ struct TriangleTopRight: Shape {
         
         //Define path
         path.move(to: CGPoint(x: rect.maxX, y: rect.minY))
-        
         path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
         path.addLine(to: CGPoint(x: rect.minX, y: rect.minY))
         path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
--- GenrativeArt/views/TitleView.swift
+++ GenrativeArt/views/TitleView.swift
@@ -9,21 +9,46 @@ import SwiftUI
 
 struct TitleView: View {
     
+    
+//    Stored properties
+    
+    let markerOne = Color.red
+    let markerTwo  = Color.orange
+    
+    let flipForColor = Coin.flip()
     let flipOne = Coin.flip()
     
+    var colorOne : Color {
+        if flipForColor == .heads {
+            return markerOne
+        } else {
+            return markerTwo
+        }
+    }
+    
+    
+    var colorTwo : Color {
+        if flipForColor == .heads {
+            return markerTwo
+        } else {
+            return markerOne
+        }
+    }
+    
+//    Computed Properties
     var body: some View {
         ZStack{
             
             if flipOne == .heads {
             TriangleTopRight()
                     .stroke(.black)
-                    .fill(.clear)
+                    .fill(colorOne)
                     .aspectRatio(1.0, contentMode: .fit)
                 
             
                 TriangleBottomLeft()
                     .stroke(.black)
-                    .fill(.clear)
+                    .fill(colorTwo)
                     .aspectRatio(1.0, contentMode: .fit)
                 
             } else {
