f40b94f6668401e4cbec6c94462d94f5ba4a59f5
Lawrence Kang <lkang@lcs.on.ca>
2023-10-28 10:30:49 -0400

    completed code with slider

--- GenrativeArt/views/GridView.swift
+++ GenrativeArt/views/GridView.swift
@@ -10,8 +10,8 @@ import SwiftUI
 struct GridView: View {
     
 //    Stored properties
-    let columns = 7
-    let rows = 4
+    @State var columns = 7.0
+    @State var rows = 4
     
     
 //    Computed properties
@@ -20,20 +20,23 @@ struct GridView: View {
         VStack {
             Grid(horizontalSpacing: 0, verticalSpacing: 0) {
     //            Number of Rows
-                ForEach(0..<rows, id: \.self){ j in
+                ForEach(0..<Int(rows), id: \.self){ j in
                     
                     
                     
                     GridRow{
                         
     //                    Repeats within row
-                        ForEach(0..<columns, id: \.self) {i in
+                        ForEach(0..<Int(columns), id: \.self) {i in
                             TitleView()
                         }
                     }
                     
                 }
             }
+            
+            
+            Slider(value: $columns, in: 1...10, step: 1.0)
         }
     }
 }
